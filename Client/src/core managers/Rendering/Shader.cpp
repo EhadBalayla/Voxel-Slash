656adf75cec3fde3ee09bf3f8117fef10353995d
Your Name <you@example.com>
2026-03-22 20:30:58 +0200

    just fixed it in the client itself

--- Client/src/core managers/Rendering/Shader.cpp
+++ Client/src/core managers/Rendering/Shader.cpp
@@ -165,6 +165,15 @@ void Shader::LoadShader(const char* vertexPath, const char* fragmentPath, Pipeli
 	colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
 	colorBlendAttachment.blendEnable = VK_FALSE;
 
+	colorBlendAttachment.blendEnable = type == PipelineType::UIShader ? VK_TRUE : VK_FALSE;
+    colorBlendAttachment.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA;
+    colorBlendAttachment.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA;
+    colorBlendAttachment.colorBlendOp = VK_BLEND_OP_ADD;
+
+    colorBlendAttachment.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
+    colorBlendAttachment.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
+    colorBlendAttachment.alphaBlendOp = VK_BLEND_OP_ADD;
+
 	VkPipelineColorBlendAttachmentState attachments[] = { colorBlendAttachment };
 
 	VkPipelineColorBlendStateCreateInfo colorBlendState{};
@@ -208,7 +217,7 @@ void Shader::LoadShader(const char* vertexPath, const char* fragmentPath, Pipeli
 	pipelineInfo.pViewportState = &viewportState;
 	pipelineInfo.pColorBlendState = &colorBlendState;
 	pipelineInfo.pDepthStencilState = &depthStencilState;
-	pipelineInfo.layout = GApp->m_Renderer.GetChunksPipelineLayout();
+	pipelineInfo.layout = type == PipelineType::UIShader ? GContext->GetSingleTexPPLayout() : GApp->m_Renderer.GetChunksPipelineLayout();
 	pipelineInfo.renderPass = GApp->m_Renderer.GetOffscreenRenderPass();
 	pipelineInfo.subpass = 0;
 
